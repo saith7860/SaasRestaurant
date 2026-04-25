bb3f5026f05556fab345f79c0823d6e80f345dfb
Muhammad Shaheer Zafar <shaheerzafar592@gmail.com>
2026-04-25 16:05:55 +0500

    Backend part is almost done.Admin is created and now admin can add categories add update delete items

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUser,loginUser } from '../controllers/userController.js';
+import {  createUser,loginUser } from '../controllers/userController.js';
 import validate from '../middlewares/validationMiddleware.js';
 import { loginSchemaZod, userSchemaZod } from '../validators/userValidator.js';
 const UserRouter =express.Router();
