0f34bbb5b02faf824e6837a9404e520189a55bf5
Muhammad Shaheer Zafar <shaheerzafar592@gmail.com>
2026-05-08 09:16:09 +0500

    Checout page is added

--- frontend/src/pages/Checkout/Checkout.tsx
+++ frontend/src/pages/Checkout/Checkout.tsx
@@ -1,12 +1,112 @@
 
-import { useContext } from "react";
+import { useContext,useState } from "react";
 import { CartContext } from "../../CartContext";
 const Checkout = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    phone: "",
+    address: "",
+    paymentMethod: "",
+  });
     const {cart}=useContext(CartContext)!;
     console.log(cart);
-    
+    const handleChange = (e:any) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  // Handle Form Submit
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
   return (
-    <div>Checkout</div>
+    <>
+    <div><form onSubmit={handleSubmit}>
+
+      {/* Full Name */}
+      <div>
+        <label>Full Name</label>
+
+        <input
+          type="text"
+          name="fullName"
+          value={formData.fullName}
+          onChange={handleChange}
+        />
+      </div>
+
+      {/* Email */}
+      <div>
+        <label>Email</label>
+
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+
+      {/* Phone */}
+      <div>
+        <label>Phone Number</label>
+
+        <input
+          type="text"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+        />
+      </div>
+
+      {/* Address */}
+      <div>
+        <label>Full Address</label>
+
+        <textarea
+          name="address"
+          value={formData.address}
+          onChange={handleChange}
+        />
+      </div>
+
+      {/* Payment Method */}
+      <div>
+        <h3>Payment Method</h3>
+
+        <label>
+          <input
+            type="radio"
+            name="paymentMethod"
+            value="cash-on-delivery"
+            checked={
+              formData.paymentMethod === "cash-on-delivery"
+            }
+            onChange={handleChange}
+          />
+
+          Cash on Delivery
+        </label>
+
+      </div>
+
+      {/* Submit */}
+      <button type="submit">
+        Place Order
+      </button>
+
+    </form></div>
+    <div></div>
+    
+
+  </>
   )
 }
 
