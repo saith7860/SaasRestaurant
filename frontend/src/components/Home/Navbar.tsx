5f2bc373a7eed2e3054cad834f7bda09b1e55c42
Dev Umair <devumair00@gmail.com>
2026-07-12 23:37:43 +0500

    Add Number badge on Cart Icon

--- frontend/src/components/Home/Navbar.tsx
+++ frontend/src/components/Home/Navbar.tsx
@@ -1,9 +1,20 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 import { Search, ShoppingCart } from "lucide-react";
 import type { FC } from "react";
 import type { NavbarProps } from "../../types/HomePageTypes";
 import { Link } from "react-router";
 
 const Navbar: FC<NavbarProps> = ({ restaurnatName, search, setSearch }) => {
+
+  const { cart } = useContext(CartContext)!;
+
+
+  const totalItems = cart.reduce(
+    (acc, item) => acc + item.quantity,
+    0
+  );
+
   return (
     <nav className="sticky top-0 z-50 w-full bg-[var(--background-color)]/95 backdrop-blur-lg border-b border-[var(--primary-color)]/15 shadow-lg px-4 py-3">
       <div className="max-w-7xl mx-auto flex items-center justify-between gap-3">
@@ -30,9 +41,17 @@ const Navbar: FC<NavbarProps> = ({ restaurnatName, search, setSearch }) => {
         {/* Right Side */}
         <div className="flex items-center gap-2 sm:gap-3">
           <Link to={"/cart"}>
-            <button className="flex items-center gap-2 bg-[var(--card-color)] border border-[var(--primary-color)]/20 text-[var(--text-color)] px-4 py-2 rounded-xl hover:bg-[var(--primary-color)] hover:text-[var(--background-color)] hover:border-[var(--primary-color)] transition-all duration-300">
+            <button className="relative flex items-center gap-2 bg-[var(--card-color)] border border-[var(--primary-color)]/20 text-[var(--text-color)] px-4 py-2 rounded-xl hover:bg-[var(--primary-color)] hover:text-[var(--background-color)] hover:border-[var(--primary-color)] transition-all duration-300">
               <ShoppingCart size={18} />
               <span className="hidden sm:inline">Cart</span>
+
+              {totalItems > 0 && (
+                <span className="absolute -right-2 -top-2 flex h-6 min-w-6 items-center justify-center rounded-full bg-[var(--primary-color)] px-1 text-xs font-bold text-[var(--background-color)] shadow-lg ring-2 ring-[var(--card-color)]">
+                  {totalItems}
+                </span>
+              )}
+
+
             </button>
           </Link>
 
