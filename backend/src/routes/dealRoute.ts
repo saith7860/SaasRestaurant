1f3f5e20b61df2e21bb34e668d9ed67ce99eefa2
Muhammad Shaheer Zafar <shaheerzafar592@gmail.com>
2026-07-26 11:16:57 +0500

    Sequence of functionality is changed in router file

--- backend/src/routes/dealRoute.ts
+++ backend/src/routes/dealRoute.ts
@@ -4,9 +4,9 @@ import validate from '../middlewares/validationMiddleware.js'
 import {createDealSchema} from '../validators/dealValidator.js';
 import * as dealController from '../controllers/dealController.js';
 const dealRouter=express.Router();
-dealRouter.post("/create-deal",validate(createDealSchema),authMiddleware,checkAdmin,dealController.createDeal)  //CREATE A NEW DEAL
+dealRouter.post("/create-deal",authMiddleware,checkAdmin,validate(createDealSchema),dealController.createDeal)  //CREATE A NEW DEAL
 dealRouter.get("/deals",dealController.getAllDeals) //GET ALL DEALS
 dealRouter.get("/deal/:id",dealController.getSpecificDeal) //GET SPECIFIC DEAL
 dealRouter.put("/update-deal/:id",authMiddleware,checkAdmin,validate(createDealSchema),dealController.updateSpecificDeal) //UPDATE SPECIFIC DEAL
 dealRouter.delete("/delete-deal/:id",authMiddleware,checkAdmin,dealController.deleteSpecificDeal) //DELETE SPECIFIC DEAL
-export default dealRouter
+export default dealRouter;
