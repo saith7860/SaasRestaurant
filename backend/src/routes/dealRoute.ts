53c7c859f77dacc3abea1d4fcbe1eae3edfd07fb
Muhammad Shaheer Zafar <shaheerzafar592@gmail.com>
2026-07-26 12:05:20 +0500

    image validation through middleware is added

--- backend/src/routes/dealRoute.ts
+++ backend/src/routes/dealRoute.ts
@@ -3,10 +3,12 @@ import {authMiddleware,checkAdmin} from '../tokens/jwt.js'
 import validate from '../middlewares/validationMiddleware.js'
 import {createDealSchema} from '../validators/dealValidator.js';
 import * as dealController from '../controllers/dealController.js';
+import { uploadSingleImage } from '../middlewares/uploadMiddleware.js';
+import { validateImage } from '../validators/validateItem.js';
 const dealRouter=express.Router();
-dealRouter.post("/create-deal",authMiddleware,checkAdmin,validate(createDealSchema),dealController.createDeal)  //CREATE A NEW DEAL
+dealRouter.post("/create-deal",authMiddleware,checkAdmin,uploadSingleImage,validate(createDealSchema),validateImage,dealController.createDeal)  //CREATE A NEW DEAL
 dealRouter.get("/deals",dealController.getAllDeals) //GET ALL DEALS
 dealRouter.get("/deal/:id",dealController.getSpecificDeal) //GET SPECIFIC DEAL
-dealRouter.put("/update-deal/:id",authMiddleware,checkAdmin,validate(createDealSchema),dealController.updateSpecificDeal) //UPDATE SPECIFIC DEAL
+dealRouter.put("/update-deal/:id",authMiddleware,checkAdmin,uploadSingleImage,validate(createDealSchema),validateImage,dealController.updateSpecificDeal) //UPDATE SPECIFIC DEAL
 dealRouter.delete("/delete-deal/:id",authMiddleware,checkAdmin,dealController.deleteSpecificDeal) //DELETE SPECIFIC DEAL
 export default dealRouter;
--- backend/src/validators/validateItem.ts
+++ backend/src/validators/validateItem.ts
@@ -1,6 +1,6 @@
 import { Request,Response,NextFunction } from "express";
 import { ApiError } from "../middlewares/errorHandler.js";
-export const validateItem = (req:Request, res:Response, next:NextFunction) => {
+export const validateImage = (req:Request, res:Response, next:NextFunction) => {
   if (!req.file) {
     throw new ApiError(400,"Image file is required")
   }
