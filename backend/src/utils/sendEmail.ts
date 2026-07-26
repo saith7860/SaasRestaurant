15d9bc8ff673c75b6253d0317889f777ecf4f0e4
Muhammad Shaheer Zafar <shaheerzafar592@gmail.com>
2026-07-26 11:30:43 +0500

    email configuration is changed from resend to nodemailer

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -1,36 +1,4 @@
-// import nodemailer from "nodemailer";
-
-// interface SendEmailOptions {
-//   to: string;
-//   subject: string;
-//   html: string;
-// }
-
-// const transporter = nodemailer.createTransport({
-//   host: process.env.EMAIL_HOST,
-//   port: Number(process.env.EMAIL_PORT),
-//   secure: false,
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
-
-// export const sendEmail = async ({ to, subject, html }: SendEmailOptions) => {
-//   try {
-//     await transporter.sendMail({
-//       from: process.env.EMAIL_FROM,
-//       to,
-//       subject,
-//       html,
-//     });
-
-//     console.log(`Email sent successfully to ${to}`);
-//   } catch (error) {
-//     console.log("Email sending failed:", error);
-//   }
-// };
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 
 interface SendEmailOptions {
   to: string;
@@ -38,25 +6,57 @@ interface SendEmailOptions {
   html: string;
 }
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
-export const sendEmail = async ({
-  to,
-  subject,
-  html,
-}: SendEmailOptions) => {
+export const sendEmail = async ({ to, subject, html }: SendEmailOptions) => {
   try {
-    const { data, error } = await resend.emails.send({
-      from:process.env.EMAIL_FROM as string,
+    await transporter.sendMail({
+      from: process.env.EMAIL_FROM,
       to,
       subject,
       html,
     });
-    if (error) {
-      console.error("Email sending failed:", error);
-    }
-    return data;
+
+    console.log(`Email sent successfully to ${to}`);
   } catch (error) {
-    console.error("Email sending failed:", error);
+    console.log("Email sending failed:", error);
   }
 };
+// import { Resend } from "resend";
+
+// interface SendEmailOptions {
+//   to: string;
+//   subject: string;
+//   html: string;
+// }
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export const sendEmail = async ({
+//   to,
+//   subject,
+//   html,
+// }: SendEmailOptions) => {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from:process.env.EMAIL_FROM as string,
+//       to,
+//       subject,
+//       html,
+//     });
+//     if (error) {
+//       console.error("Email sending failed:", error);
+//     }
+//     return data;
+//   } catch (error) {
+//     console.error("Email sending failed:", error);
+//   }
+// };
