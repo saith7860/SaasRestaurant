efafd760805b099ebb2de6a3062d3cf00f1b0a54
Muhammad Shaheer Zafar <shaheerzafar592@gmail.com>
2026-07-30 11:25:26 +0500

    Package lock json file is newly created due to merge conflicts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from "@cloudflare/vite-plugin";
 // https://vite.dev/config/
 export default defineConfig({
   server:{
@@ -15,7 +14,7 @@ export default defineConfig({
       },
     }
   },
-  plugins: [react(), tailwindcss(), cloudflare()],
+  plugins: [react(), tailwindcss()],
 
   
 })
