10f71cd4c1ac2b73cd46ac961ad67e62bf498a35
Dev Umair <devumair00@gmail.com>
2026-08-03 20:01:40 +0500

    Add Searchbar Component

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "../../components/Home/Navbar"
 import Hero from "../../components/Home/Hero/Hero";
+import SearchBar from "../../components/Home/SearchBar";
 import { useRestaurant } from "../../context/RestaurantContext";
 import SpecificCatogires from "../../components/Home/SpecificCatogires";
 import WhatsAppButton from "../../components/Home/WhatsppButton";
@@ -13,6 +14,7 @@ const Home = () => {
     <div className="overflow-x-hidden">
      <Navbar restaurnatName={restaurantData?.restaurantData?.restaurantName||null} setSearch={setSearch}   search={search}/>
      <Hero/>
+     <SearchBar/>
      <SpecificCatogires search={search}/>
      <WhatsAppButton/>
      {/* <Categories search={search}/> */}
